9edd253a7ca118824dcdf37a30fd38d4c5f57c2f
Ben Mercer <ben@henroben.co.uk>
2017-04-27 12:07:20 +0100

    recharts added

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,19 +1,23 @@
 import _ from 'lodash';
 import React from 'react';
-import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
+// import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
 
-function average(data) {
-    return _.round(_.sum(data)/data.length);
-}
+// function average(data) {
+//     return _.round(_.sum(data)/data.length);
+// }
 
 export default (props) => {
     return(
-        <div>
-            <Sparklines data={ props.chartData } svgWidth={200} svgHeight={100} >
-                <SparklinesLine color={ props.chartColour } />
-                <SparklinesReferenceLine type="avg" />
-            </Sparklines>
-            <div></div>
+        <div className="chart">
+                <AreaChart width={800} height={400} data={props.data}
+                           margin={{top: 10, right: 30, left: 0, bottom: 0}}>
+                    <XAxis dataKey="name"/>
+                    <YAxis/>
+                    <CartesianGrid strokeDasharray="3 3"/>
+                    <Tooltip/>
+                    <Area type='monotone' dataKey={props.units} stroke='#8884d8' fill='#8884d8' />
+                </AreaChart>
         </div>
     );
 }
--- src/components/display_daily_nutrients.js
+++ src/components/display_daily_nutrients.js
@@ -1,7 +1,61 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
 import Chart from '../components/chart';
 
 export default class DisplayDailyNutrients extends Component {
+    calcMineralTotals(nutrients) {
+        let mineralsForChart = [];
+        let minerals = _.filter(nutrients, (nutrient) => {
+            return nutrient.group === 'Minerals';
+        });
+        minerals.map((mineral) => {
+            mineralsForChart.push({
+                name: mineral.name,
+                mg: mineral.measures[0].value
+            });
+        });
+        return mineralsForChart;
+    }
+    calcVitaminsTotals(nutrients) {
+        let vitaminsForChart = [];
+        let vitamins = _.filter(nutrients, (vitamin) => {
+            return vitamin.group === 'Vitamins';
+        });
+        vitamins.map((vitamin) => {
+            vitaminsForChart.push({
+                name: vitamin.name,
+                mg: vitamin.measures[0].value
+            });
+        });
+        return vitaminsForChart;
+    }
+    calcLipidsTotals(nutrients) {
+        let lipidsForChart = [];
+        let lipids = _.filter(nutrients, (lipid) => {
+            return lipid.group === 'Lipids';
+        });
+        lipids.map((lipid) => {
+            lipidsForChart.push({
+                name: lipid.name,
+                g: lipid.measures[0].value
+            });
+        });
+        return lipidsForChart;
+    }
+    calcProximatesTotals(nutrients) {
+        let proximatesForChart = [];
+        let proximates = _.filter(nutrients, (nutrient) => {
+            return nutrient.group === 'Proximates' && nutrient.unit !== 'kcal';
+        });
+        proximates.map((proximate) => {
+            proximatesForChart.push({
+                name: proximate.name,
+                g: proximate.measures[0].value
+            });
+        });
+        return proximatesForChart;
+    }
+
     renderNutrientTotals(nutrients) {
         return nutrients.map((nutrient) => {
             return(
@@ -14,14 +68,26 @@ export default class DisplayDailyNutrients extends Component {
         })
     }
     render() {
-        return(
-            <div className="panel panel-default">
-                <div className="panel-heading"><h5>Daily Nutrient Totals</h5></div>
-                <div className="panel-body">
-                    {this.renderNutrientTotals(this.props.nutrients)}
-                    <Chart chartData={ [1,5,7] } chartColour={ 'orange' } chartUnits={'mg'} />
+        if(this.props.nutrients.length != 0) {
+            return(
+                <div className="panel panel-default">
+                    <div className="panel-heading"><h5>Daily Nutrient Totals</h5></div>
+                    <div className="panel-body">
+                        {/*{this.renderNutrientTotals(this.props.nutrients)}*/}
+                        <h5>Proximates</h5>
+                        <Chart data={this.calcProximatesTotals(this.props.nutrients)} units={'g'} />
+                        <h5>Minerals</h5>
+                        <Chart data={this.calcMineralTotals(this.props.nutrients)} units={'mg'} />
+                        <h5>Vitamins</h5>
+                        <Chart data={this.calcVitaminsTotals(this.props.nutrients)} units={'mg'} />
+                        <h5>Lipids</h5>
+                        <Chart data={this.calcLipidsTotals(this.props.nutrients)} units={'g'} />
+                    </div>
                 </div>
-            </div>
-        );
+            );
+        } else {
+            return(<div></div>);
+        }
+
     }
 }
